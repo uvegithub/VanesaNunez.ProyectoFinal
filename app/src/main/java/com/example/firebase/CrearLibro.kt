8c72594225f7445539343a25ef15f368b0f5e548
Vanesa Nunez <vanesanunez90dos@gmail.com>
2024-03-07 20:29:20 +0100

    actu 7_3

---
+++ app/src/main/java/com/example/firebase/CrearLibro.kt
@@ -0,0 +1,11 @@
+package com.example.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearLibro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_libro)
+    }
+}
